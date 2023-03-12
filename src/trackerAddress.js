fe82dd7d5993452fbdd67bf3a4acfa8a7ba7276d
zhijie.xia <zhijiexiacs@gmail.com>
2023-03-12 01:02:19 -0700

    three trackers

--- src/trackerAddress.js
+++ src/trackerAddress.js
@@ -1,6 +1,7 @@
 const tracker_locations = {
     trackers : [
         {ip: "192.168.0.20", port:"3001"},
+        {ip: "192.168.0.20", port:"3002"},
         {ip: "192.168.0.14" , port:"3001"}
     ]
 }
